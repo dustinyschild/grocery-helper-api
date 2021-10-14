009adeec866196bfbc0b6dc2abd465c253702176
Dustin Yelland <dyelland@gmail.com>
2021-10-13 20:24:51 -0500

    rename registration test file

--- test/register.test.js
+++ test/register.test.js
@@ -6,6 +6,14 @@ describe("User registration", async function () {
     // arrange
     // db connection already established
     // act
+    request
+      .post("/api/user")
+      .send({
+        username: "xkcd",
+        email: "xkcd@example.com",
+        password: "correct horse battery staple"
+      })
+      .expect(200);
     // assert
   });
 });
